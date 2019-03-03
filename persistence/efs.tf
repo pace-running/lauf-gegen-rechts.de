338956d1e38048d4e48e617b960a16e233657e7e
Philipp Eckel <peckel@thoughtworks.com>
2019-03-03 18:11:37 +0100

    PE adding security group to EFS mount targets

--- persistence/efs.tf
+++ persistence/efs.tf
@@ -3,13 +3,15 @@ resource "aws_efs_file_system" "pdf-efs" {
 }
 
 resource "aws_efs_mount_target" "pdf-efs-target-subnet-1" {
-  file_system_id = "${aws_efs_file_system.pdf-efs.id}"
-  subnet_id      = "${element(data.aws_subnet_ids.ec2_subnets.ids, 1)}"
+  file_system_id  = "${aws_efs_file_system.pdf-efs.id}"
+  subnet_id       = "${element(data.aws_subnet_ids.ec2_subnets.ids, 1)}"
+  security_groups = ["${aws_security_group.efs-security-group.id}"]
 }
 
 resource "aws_efs_mount_target" "pdf-efs-target-subnet-2" {
-  file_system_id = "${aws_efs_file_system.pdf-efs.id}"
-  subnet_id      = "${element(data.aws_subnet_ids.ec2_subnets.ids, 2)}"
+  file_system_id  = "${aws_efs_file_system.pdf-efs.id}"
+  subnet_id       = "${element(data.aws_subnet_ids.ec2_subnets.ids, 2)}"
+  security_groups = ["${aws_security_group.efs-security-group.id}"]
 }
 
 data "aws_subnet_ids" "ec2_subnets" {
@@ -17,5 +19,28 @@ data "aws_subnet_ids" "ec2_subnets" {
 
   tags {
     "Name" = "pace-ec2-subnet"
+  }
+}
+
+resource "aws_security_group" "efs-security-group" {
+  name = "efs-security-group"
+
+  description = "Allow access to EFS"
+  vpc_id      = "${var.vpc-id}"
+
+  # Only NFS in
+  ingress {
+    from_port       = 2049
+    to_port         = 2049
+    protocol        = "tcp"
+    security_groups = ["${var.security-group-id}"]
+  }
+
+  # Allow all outbound traffic.
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
